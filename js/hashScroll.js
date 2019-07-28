d2e0c1f28d72a8aaf786d0ce189da70b21f4e79d
Studio Praktik <studio@praktik.co.il>
2019-07-28 10:37:32 +0300

    adding setHash parameter

--- js/hashScroll.js
+++ js/hashScroll.js
@@ -1,5 +1,5 @@
 const hashScroll = () => {
-  const scrollToTarget = (targetElement, directions = { top: true, bottom: true }) => {
+  const scrollToTarget = (targetElement, setHash = true, directions = { top: true, bottom: true }) => {
     const headerOffset = document.querySelector('#site-header').clientHeight
     const currentPosition = window.scrollY || window.pageYOffset
     const targetHeight = targetElement.getBoundingClientRect().top + currentPosition - headerOffset
@@ -10,16 +10,18 @@ const hashScroll = () => {
 
       // set hash
       // after target is checked for effects, remove the id ...
-      setTimeout(() => {
-        const hash = targetElement.id
-        const el = document.getElementById(hash)
-        el.setAttribute('id', '')
-        location.hash = hash
-        // ... then wait for animation to finish and restore id
+      if (setHash) {
         setTimeout(() => {
-          el.setAttribute('id', hash)
-        }, 950)
-      }, 50)
+          const hash = targetElement.id
+          const el = document.getElementById(hash)
+          el.setAttribute('id', '')
+          location.hash = hash
+          // ... then wait for animation to finish and restore id
+          setTimeout(() => {
+            el.setAttribute('id', hash)
+          }, 950)
+        }, 50)
+      }
     }
   }
 
@@ -32,13 +34,13 @@ const hashScroll = () => {
     }
   }
 
-  const handleHashLink = hash => {
+  const handleHashLink = (hash, setHash) => {
     if (hash === '#') {
       window.scrollTo({ behavior: "smooth", top: 0 })
     } else {
       const targetElement = document.querySelector(hash)
       if (targetElement)
-        scrollToTarget(targetElement)
+        scrollToTarget(targetElement, setHash)
       else
         location.assign(`${location.protocol}//${location.hostname}/${hash}`) // go to hash on homepage
 
@@ -53,6 +55,7 @@ const hashScroll = () => {
     const linkEl = el.matches('a') ? el : el.closest('a')
     if (linkEl) {
       let href = linkEl.getAttribute('href')
+      const setHash = !linkEl.hasAttribute('data-unset-hash')
 
       // if absolute link to hash on page, set hash only
       if (href.indexOf('#') !== -1 && window.location.href.split('#')[0] === href.split('#')[0])
@@ -61,7 +64,7 @@ const hashScroll = () => {
       if (href.indexOf('#') === 0) {
         event.preventDefault()
         window.dispatchEvent(new Event('hashClicked'))
-        handleHashLink(href)
+        handleHashLink(href, setHash)
       }
     }
   }
