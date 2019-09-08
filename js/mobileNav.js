c5ab00399466f2b0735500a9f312d24caa98a2c6
Jonathan Land <studio@praktik.co.il>
2019-09-08 13:39:04 +0300

    mobileNav toggle

---
+++ js/mobileNav.js
@@ -0,0 +1,30 @@
+const mobileNav = (
+  $toggle = document.getElementById('nav-toggle'),
+  $header = document.getElementById('site-header')
+) => {
+  const $items = $header.querySelectorAll('li')
+  if (!$toggle || !$header) return
+
+  let open = false
+  const toggleOpen = force => {
+    open = force !== undefined ? force : !open
+    $header.classList.toggle('open', open)
+
+    if (!!window.bodyScrollLock) {
+      if (open)
+        bodyScrollLock.disableBodyScroll('#site-header')
+      else
+        bodyScrollLock.clearAllBodyScrollLocks()
+    }
+  }
+
+  $toggle.addEventListener('click', () => {
+    toggleOpen()
+  })
+
+  $items.forEach($item => {
+    $item.addEventListener('click', () => {
+      toggleOpen(false)
+    })
+  })
+}
