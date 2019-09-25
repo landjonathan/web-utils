4e631ea17d45093e7c606825ff21d86d8306d445
Jonathan Land <studio@praktik.co.il>
2019-09-25 14:18:38 +0300

    bottomInPage

--- js/helpers.js
+++ js/helpers.js
@@ -3,4 +3,5 @@ const helpers = () => {
   window.$$ = window.$$ || document.querySelectorAll.bind(document)
   window.clamp = (val, min, max) => val > max ? max : val < min ? min : val
   window.topInPage = $el => $el.getBoundingClientRect().top + window.scrollY
+  window.bottomInPage = $el => $el.getBoundingClientRect().bottom + window.scrollY
 }
