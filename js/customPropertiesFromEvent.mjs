e2d7a4575473c93d025b87c0c946a1424c083280
Jonathan Land <land.jonathan@gmail.com>
2020-06-17 13:25:12 +0300

    feat(js): customPropertiesFromEvent single event

---
+++ js/customPropertiesFromEvent.mjs
@@ -0,0 +1,31 @@
+const customPropertiesFromEvent = ({
+                                     name = 'mousemove',
+                                     values = [
+                                       { key: 'clientX', calculation: x => (x / window.innerWidth).toFixed(6) },
+                                       {
+                                         key: 'clientY',
+                                         calculation: y => (y / window.innerHeight).toFixed(6),
+                                         variable: '--client-y'
+                                       }
+                                     ],
+                                   } = {}) => {
+  const setState = event => {
+    requestAnimationFrame(() => {
+      for (let { key, variable, calculation } of values) {
+        variable = variable || '--' + key.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
+        if (event[key]) {
+          const value = calculation ? calculation(event[key]) : event[key]
+          document.documentElement.style.setProperty(variable, value)
+        }
+      }
+    })
+  }
+
+  if (window.windowListeners && window.windowListeners[name])
+    window.windowListeners[name].push(setState)
+  else
+    window.addEventListener(name, setState)
+}
+
+export default customPropertiesFromEvent
+// camel to kebab — https://gist.github.com/nblackburn/875e6ff75bc8ce171c758bf75f304707
