19c19b40d447fc2f22c3e2e5a530bd0fc1d2bcdb
Jonathan Land <land.jonathan@gmail.com>
2020-07-02 12:49:36 +0300

    feat(11ty): array sub filter

--- 11ty/dataFilters.js
+++ 11ty/dataFilters.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig, pluginNamespace) {
   eleventyConfig.namespace(pluginNamespace, () => {
     eleventyConfig.addFilter('find', (array, key, value) => array.find(x => x[key] === value))
     eleventyConfig.addFilter('filter', (array, key, value) => array.filter(x => x[key] === value))
+    eleventyConfig.addFilter('sub', (array, n, start = 0) => array.slice(start, n))
     eleventyConfig.addFilter('dataFromSlug', (array, slug) => array.find(x => x.fileSlug && x.fileSlug === slug).data)
   })
 }
