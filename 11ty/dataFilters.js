53f9a05682ca9cfccaeca97052cb09b1759b2779
Jonathan Land <land.jonathan@gmail.com>
2020-07-02 17:16:38 +0300

    feat(11ty): array map filter

--- 11ty/dataFilters.js
+++ 11ty/dataFilters.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig, pluginNamespace) {
     eleventyConfig.addFilter('find', (array, key, value) => array.find(x => x[key] === value))
     eleventyConfig.addFilter('filter', (array, key, value) => array.filter(x => x[key] === value))
     eleventyConfig.addFilter('sub', (array, n, start = 0) => array.slice(start, n))
+    eleventyConfig.addFilter('map', (array, key) => array.map(n => n[key]))
     eleventyConfig.addFilter('dataFromSlug', (array, slug) => array.find(x => x.fileSlug && x.fileSlug === slug).data)
   })
 }
