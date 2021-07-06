14d0b484f76072ea263e4710b8439b30fe2ff80a
Jonathan Land <land.jonathan@gmail.com>
2021-07-06 16:23:29 +0300

    feat(vue): praktik credit removing `functional` to support vue 3

--- vue/PraktikCredit.vue
+++ vue/PraktikCredit.vue
@@ -1,4 +1,4 @@
-<template functional>
+<template>
   <p class="praktik-credit"><slot>Designed and built by</slot><a href="https://praktik.design" target="_blank" rel="noreferrer">
     <svg
       version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
